b9575fc57e2a2e433eed3bf15186fe537b572171
ArneCode <arne.deborman@web.de>
2022-03-30 12:00:23 +0000

    added c/cpp extension, added run task

--- src/main.cpp
+++ src/main.cpp
@@ -2,27 +2,30 @@
 #include <emscripten.h>
 #include <iostream>
 
-extern "C"{
+extern "C"
+{
   extern void console_log(std::string text);
 }
 float mal2(float n) { return n * 2; }
 
 EMSCRIPTEN_BINDINGS(my_module) { emscripten::function("mal2", &mal2); }
-class Mensch {
+class Mensch
+{
   int alter;
 
 public:
   Mensch(int alter) : alter(alter) {}
   int getAlter() { return alter; }
   void setAlter(int n) { alter = n; }
-  void sagHallo() {
-    std::cout<<"Hallo Welt, ich bin "<<alter<<" Jahre alt\n";
-   }
+  void sagHallo()
+  {
+    std::cout << "Hallo Welt, ich bin " << alter << " Jahre alt\n";
+  }
 };
-EMSCRIPTEN_BINDINGS(my_class_example) {
+EMSCRIPTEN_BINDINGS(my_class_example)
+{
   emscripten::class_<Mensch>("Mensch")
       .constructor<int>()
       .function("getAlter", &Mensch::getAlter)
-      .function("sagHallo", &Mensch::sagHallo)
-      ;
+      .function("sagHallo", &Mensch::sagHallo);
 }
